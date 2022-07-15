1f2f24774dad4a1b94e4474a54201eafa412f357
Yerkegali Zhamalbekov <87934692+Yerkeboss@users.noreply.github.com>
2022-07-15 10:57:26 +0600

    First commit

---
+++ build/precache-manifest.c63d3d581a3601fab8421f7af5e49712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3e6efd27cc588b09eb7cf403433c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3904d4b710de92e87ce0",
+    "url": "/static/css/main.69ebc593.chunk.css"
+  },
+  {
+    "revision": "314bf1fb5cbdce0929f2",
+    "url": "/static/js/2.7a6371c3.chunk.js"
+  },
+  {
+    "revision": "68481d214dfda62d485a39c2c450691e",
+    "url": "/static/js/2.7a6371c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3904d4b710de92e87ce0",
+    "url": "/static/js/main.06bd99ff.chunk.js"
+  },
+  {
+    "revision": "2e64c12e884cf16c1acc",
+    "url": "/static/js/runtime-main.b84a8c45.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import Toucan from "./Toucan.js";
+import Squirrel from "./Squirrel.js";
+import Fox from "./Fox.js";
+import Land from "./Land";
+import Sphere from "./Animated-sphere";
+import { OrbitControls } from "@react-three/drei/OrbitControls";
+import "./styles.css";
+import Tree1 from "./Tree1.js";
+import Tree2 from "./Tree2.js";
+import Tree3 from "./Tree3.js";
+import * as THREE from 'three';
+// import Test from "./test";
+
+
+
+var camera;
+camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+camera.position.set(100, 100, 100);
+
+export default function App() {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <ambientLight intensity={0.6} />
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={null}>
+        <Toucan />
+        <Squirrel/>
+        <Fox/>
+        <Land/>
+        <Tree1/>
+        <Tree2/>
+        <Tree3/>
+        {/* <Test/> */}
+      </Suspense>
+    </Canvas>
+  );
+}
